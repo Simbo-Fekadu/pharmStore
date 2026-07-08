198606a65bc8a0bc31919dacfb713a478f53c679
Simbo <simbofekadu2629@gmail.com>
2026-07-08 15:24:57 +0300

    refactor: update routing structure to enhance component organization and clarity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import { isElectron } from "./api/base";
+import AdminLayout from "./components/AdminLayout";
+import EmployeeLayout from "./components/EmployeeLayout";
 import Landing from "./components/Landing";
 import SignIn from "./components/SignIn";
 import InventoryPage from "./components/InventoryPage";
-import AdminLayout from "./components/AdminLayout";
+import Dashboard from "./components/Dashboard";
 import AdminMedicines from "./components/AdminMedicines";
-import AdminMedicineAdd from "./components/AdminMedicineAdd";
+import MedicineAdd from "./components/MedicineAdd";
 import AdminActiveMedicines from "./components/AdminActiveMedicines";
-import AdminDashboard from "./components/AdminDashboard";
 import AdminMedicineTrash from "./components/AdminMedicineTrash";
-import AdminNearExpiry from "./components/AdminNearExpiry";
-import AdminExpired from "./components/AdminExpired";
+import MedicineExpiry from "./components/MedicineExpiry";
 import AdminInventory from "./components/AdminInventory";
 import AdminUsers from "./components/AdminUsers";
 import AdminSuppliers from "./components/AdminSuppliers";
@@ -20,12 +20,9 @@ import Fulfillment from "./components/Fulfillment";
 import AdminRequestCenter from "./components/AdminRequestCenter";
 import AdminTransactions from "./components/AdminTransactions";
 import AdminSales from "./components/AdminSales";
-import EmployeeLayout from "./components/EmployeeLayout";
-import EmployeeDashboard from "./components/EmployeeDashboard";
 import EmployeeMedicines from "./components/EmployeeMedicines";
 import BranchMedicines from "./components/BranchMedicines";
 import EmployeeAddStock from "./components/EmployeeAddStock";
-import EmployeeMedicineAdd from "./components/EmployeeMedicineAdd";
 import EmployeeSales from "./components/EmployeeSales";
 import EmployeeSalesHistory from "./components/EmployeeSalesHistory";
 import Chat from "./components/Chat";
@@ -47,46 +44,30 @@ function App() {
             <Route path="/" element={<Landing />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/inventory" element={<InventoryPage />} />
-            {/* Employee nested functionality moved under /employee */}
             <Route path="/admin" element={<AdminLayout />}>
-              <Route index element={<AdminDashboard />} />
+              <Route index element={<Dashboard />} />
               <Route path="medicines" element={<AdminMedicines />} />
-              <Route path="medicines/add" element={<AdminMedicineAdd />} />
-              <Route
-                path="medicines/active"
-                element={<AdminActiveMedicines />}
-              />
+              <Route path="medicines/add" element={<MedicineAdd role="admin" />} />
+              <Route path="medicines/active" element={<AdminActiveMedicines />} />
               <Route path="medicines/trash" element={<AdminMedicineTrash />} />
-              <Route
-                path="medicines/near-expiry"
-                element={<AdminNearExpiry />}
-              />
-              <Route path="medicines/expired" element={<AdminExpired />} />
+              <Route path="medicines/near-expiry" element={<MedicineExpiry type="near" />} />
+              <Route path="medicines/expired" element={<MedicineExpiry type="expired" />} />
               <Route path="inventory/store" element={<AdminInventory />} />
-              <Route
-                path="inventory/branches"
-                element={<AdminRequestCenter />}
-              />
+              <Route path="inventory/branches" element={<AdminRequestCenter />} />
               <Route path="chat" element={<Chat />} />
               <Route path="sales" element={<AdminSales />} />
               <Route path="transactions" element={<AdminTransactions />} />
               <Route path="users" element={<AdminUsers />} />
               <Route path="suppliers" element={<AdminSuppliers />} />
               <Route path="branches" element={<AdminBranches />} />
-              <Route
-                path="pharmacies/:id"
-                element={<SuperAdminPharmacyDetail />}
-              />
-              <Route
-                path="pharmacies/:id/branches/:branchId"
-                element={<SuperAdminBranchDetail />}
-              />
+              <Route path="pharmacies/:id" element={<SuperAdminPharmacyDetail />} />
+              <Route path="pharmacies/:id/branches/:branchId" element={<SuperAdminBranchDetail />} />
               <Route path="export" element={<ExportCenter />} />
             </Route>
             <Route path="/employee" element={<EmployeeLayout />}>
-              <Route index element={<EmployeeDashboard />} />
+              <Route index element={<Dashboard />} />
               <Route path="medicines" element={<EmployeeMedicines />} />
-              <Route path="medicines/add" element={<EmployeeMedicineAdd />} />
+              <Route path="medicines/add" element={<MedicineAdd role="employee" />} />
               <Route path="branch-medicines" element={<BranchMedicines />} />
               <Route path="add-stock" element={<EmployeeAddStock />} />
               <Route path="requests" element={<BranchRequest />} />
